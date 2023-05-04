80a30445b67eb092baccbfdbe116d70fab03d495
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-05-04 18:32:51 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,10 +56,13 @@ wget --no-check-certificate https://github.com/ponces/treble_build_evo/releases/
 
 msg "Extract GSI..."
 tar -xvf evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
+git config --global --add safe.directory /github/workspace
+
+sleep 5
+
 ls
 pwd
 msg "Convert GSI..."
-cd  evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img
 rm -Rf tmp
 mkdir -p d tmp
 e2fsck -y -f evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img
