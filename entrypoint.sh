2c11e252e518fe2ea82cc19701d897b74118c902
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-05-04 18:15:27 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ apt update && apt upgrade -y
 
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
-    curl zip wget
+    curl zip wget erofs-utils
     
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 git config --global --add safe.directory /github/workspace
@@ -48,13 +48,30 @@ git config --global --add safe.directory /github/workspace
 #sudo rm -f /etc/ssl/certs/ca-bundle.crt
 #sudo apt reinstall ca-certificates
 #sudo update-ca-certificates
+#curl --remote-name https://github.com/ponces/treble_build_evo/releases/download/v2023.05.04/evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
+
+msg "Download GSI..."
 
 wget --no-check-certificate https://github.com/ponces/treble_build_evo/releases/download/v2023.05.04/evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
 
-#curl --remote-name https://github.com/ponces/treble_build_evo/releases/download/v2023.05.04/evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
+msg "Extract GSI..."
+tar -xvf evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
+
+msg "Convert GSI..."
+
+rm -Rf tmp
+mkdir -p d tmp
+e2fsck -y -f evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img
+resize2fs evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img. 5000M
+e2fsck -E unshare_blocks -y -f evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img
+mount -o loop,rw evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img d
+sleep 1
+rm -Rf evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img
+mkfs.erofs -E legacy-compress -zlz4hc -d2 evolution_arm64-ab-vndklite-7.9-unofficial-20230503-erofs.img d/
+umount d
 
 
 
-set_output outfile evolution_arm64-ab-vndklite-7.9-unofficial-20230503.img.xz
+set_output outfile evolution_arm64-ab-vndklite-7.9-unofficial-20230503-erofs.img
 
     exit 0
